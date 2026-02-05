eabbbb3cd5ae236fbe873dc2e570910cac9fdcf5
yangshuang9889 <102595133+yangshuang9889@users.noreply.github.com>
2026-02-05 11:30:54 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,78 @@
+// 全局配置对象
+const CONFIG = {
+    // 代表作列表（占位图尺寸匹配卡片封面350x200，避免缩放变形）
+    worksList: [
+        {
+            title: "《植物大战僵尸：重植版》闪电之关气氛变诡异了（8）",
+            desc: "经典游戏重植版，闪电关卡氛围突变，东北话解说带你解锁不一样的植物防御体验！",
+            duration: "22:45",
+            view: "308",
+            comment: "22",
+            date: "2小时前",
+            cover: "https://picsum.photos/350/200?random=3", // 尺寸匹配封面容器
+            link: "https://space.bilibili.com/273715"
+        },
+        {
+            title: "《FC双截龙3：摩登时代》除了双龙兄弟，别人都提速了",
+            desc: "怀旧FC经典游戏改版，角色提速玩法升级，东北话吐槽带你重温童年热血！",
+            duration: "35:52",
+            view: "1127",
+            comment: "69",
+            date: "昨天",
+            cover: "https://picsum.photos/350/200?random=4",
+            link: "https://space.bilibili.com/273715"
+        },
+        {
+            title: "挑战天花板级找异常《异常零：12》1小时模式通关",
+            desc: "时间裂缝侵蚀的封闭空间，回溯时间改变过去，东北话解说带你破解烧脑谜题！",
+            duration: "01:11:17",
+            view: "557",
+            comment: "40",
+            date: "昨天",
+            cover: "https://picsum.photos/350/200?random=5",
+            link: "https://space.bilibili.com/273715"
+        },
+        {
+            title: "《白色情人节2：谎言之花》第2期 哈利波特哥大战恐怖警卫",
+            desc: "延都高中奇异现象频发，着魔警察四处游荡，东北话解说带你探索隐藏真相！",
+            duration: "23:57",
+            view: "904",
+            comment: "59",
+            date: "2天前",
+            cover: "https://picsum.photos/350/200?random=6",
+            link: "https://space.bilibili.com/273715"
+        },
+        {
+            title: "《植物大战僵尸：杂交版》148 荷叶筐狗加五星磁铁",
+            desc: "创意杂交玩法，荷叶筐狗搭配五星磁铁，东北话解说解锁植物新组合！",
+            duration: "13:19",
+            view: "940",
+            comment: "55",
+            date: "3天前",
+            cover: "https://picsum.photos/350/200?random=7",
+            link: "https://space.bilibili.com/273715"
+        },
+        {
+            title: "韩国经典恐怖游戏续作《白色情人节2：谎言之花》第1期",
+            desc: "封闭学校暗藏玄机，奇异现象背后的真相，东北话解说带你勇闯恐怖校园！",
+            duration: "24:00",
+            view: "1829",
+            comment: "95",
+            date: "02月01日",
+            cover: "https://picsum.photos/350/200?random=8",
+            link: "https://space.bilibili.com/273715"
+        }
+    ],
+    // 平台链接配置
+    platformLinks: {
+        bilibili: "https://space.bilibili.com/273715",
+        youku: "https://www.youku.com/",
+        tiktok: "https://www.douyin.com/",
+        live: "https://live.bilibili.com/841456"
+    },
+    // 联系方式配置
+    contact: {
+        email: "example@xxx.com",
+        weibo: "@小握解说"
+    }
+};
